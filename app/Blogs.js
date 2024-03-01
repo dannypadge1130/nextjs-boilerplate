0cb060e5b187d8399f104d791604aadd18f009a9
Dan <dan.padgett1@gmail.com>
2024-02-29 22:01:19 -0500

    Update Blogs.js

--- app/Blogs.js
+++ app/Blogs.js
@@ -1,47 +1,52 @@
-import React, { useEffect, useState } from 'react'
+// Next.js components
+import React from 'react';
+import { gql, GraphQLClient } from 'graphql-request';
 
-const Blogs = () => {
-
-    const [blogs, setBlogs] = useState([]);
-
-    useEffect(() => {
-        fetch('https://wordpress-1223464-4354486.cloudwaysapps.com/graphql', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                query: `
-                {
-                    posts {
-                        nodes{
-                            title
-                            author {
-                                node {
-                                    name
-                                }
-                            }
-                        }
-                    }
-                }`
-            })
-        }).then(res => res.json())
-        .then(res => {
-            console.log(res)
-            setBlogs(res.data.posts.nodes)
-        })
-    }, [])
-
+export default function Blogs({ blogs }) {
     return (
         <div>
-            {blogs.map(blog =>
+            {blogs.map((blog) => (
                 <div key={blog.id}>
                     <h4>{blog.title}</h4>
                     <p>Author: {blog.author.node.name}</p>
                 </div>
-            )}
+            ))}
         </div>
     );
 }
 
-export default Blogs
+// This function runs at build time in production
+// and will pre-render the page with the blogs data
+export async function getStaticProps() {
+    const graphQLClient = new GraphQLClient('https://wordpress-1223464-4354486.cloudwaysapps.com/graphql', {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const query = gql`
+        {
+            posts {
+                nodes {
+                    id
+                    title
+                    author {
+                        node {
+                            name
+                        }
+                    }
+                }
+            }
+        }
+    `;
+
+    const data = await graphQLClient.request(query);
+    const blogs = data.posts.nodes;
+
+    return {
+        props: {
+            blogs,
+        },
+        revalidate: 10, // In seconds, you can adjust this value based on how often you expect the data to change
+    };
+}
